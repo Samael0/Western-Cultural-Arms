0272167496b0a2ed1ed0919df69c6b11e388fbd6
Valtiel0 <alvaradojose08123@gmail.com>
2019-03-06 12:29:42 -0800

    Working program

--- project_1_joseAlvarado.s
+++ project_1_joseAlvarado.s
@@ -1,12 +1,12 @@
 	.data
-str1:	.asciz	"Lost Generation: %d\n"
-str2:	.asciz	"Greatest Generation: %d\n"
+str1:	.asciz	"Lost Generation:        %d\n"
+str2:	.asciz	"Greatest Generation:    %d\n"
 str3:	.asciz	"Baby Boomer Generation: %d\n"
-str4:	.asciz	"Generation X: %d\n"
-str5:	.asciz	"Generation Y: %d\n"
-str6:	.asciz	"Generation Z: %d\n"
-str7:	.asciz	"Not Applicable: %d\n"
-ary:	.word	100,40,5
+str4:	.asciz	"Generation X:           %d\n"
+str5:	.asciz	"Generation Y:           %d\n"
+str6:	.asciz	"Generation Z:           %d\n"
+str7:	.asciz	"Not Applicable:         %d\n"
+ary:	.word	100,40,5,65,78
 	.equ	arysize,(. - ary)/4
 
 	.text
@@ -15,66 +15,69 @@ ary:	.word	100,40,5
 main:	stmfd	sp!,{lr}
 	ldr	r1,=ary
 	ldr	r2,=arysize
+	mov	r5,#0
+	mov	r6,#0
+	mov	r10,#0
 	mov	r0,#0
 
 read:	cmp	r0,r2
 	beq	prtlg
-	ldr	r1,[r3]
+	ldr	r3,[r1]
 	add	r1,#4
 	add	r0,r0,#1
 	b	lg
 
-lg:	cmp	r1,#127
+lg:	cmp	r3,#127
 	ble	lg2
-	add	r12,r12,#1
+	add	r5,r5,#1
 	b	read
 
-lg2:	cmp	r1,#102
+lg2:	cmp	r3,#102
 	blt	gg
 	add	r6,r6,#1
 	b	read
 
-gg:	cmp	r1,#101
+gg:	cmp	r3,#101
 	ble	gg2		
 
-gg2:	cmp	r1,#93
+gg2:	cmp	r3,#93
 	blt	bb
 	add	r7,r7,#1
 	b	read
 
-bb:	cmp	r1,#71
+bb:	cmp	r3,#71
 	ble	bb2
 
-bb2:	cmp	r1,#53
+bb2:	cmp	r3,#53
 	blt	gx
 	add	r8,r8,#1
 	b	read
 
-gx:	cmp	r1,#52
+gx:	cmp	r3,#52
 	ble	gx2
 
-gx2:	cmp	r1,#38
+gx2:	cmp	r3,#38
 	blt	gy
 	add	r9,r9,#1
 	b	read
 
-gy:	cmp	r1,#36
+gy:	cmp	r3,#36
 	ble	gy2
 
-gy2:	cmp	r1,#22
+gy2:	cmp	r3,#22
 	blt	gz
 	add	r10,r10,#1
 	b	read
 
-gz:	cmp	r1,#21
+gz:	cmp	r3,#21
 	ble	gz2
 
-gz2:	cmp	r1,#7
+gz2:	cmp	r3,#7
 	blt	na
 	add	r11,r11,#1
 	b	read
 
-na:	add	r12,r12,#1
+na:	add	r5,r5,#1
 	b	read
 
 prtlg:	cmp	r6,#0
@@ -131,11 +134,11 @@ prtgz:	cmp	r11,#0
 	@end
 	b	prtna
 
-prtna:	cmp	r12,#0
+prtna:	cmp	r5,#0
 	beq	end
 	@print for not applicable code here
 	ldr	r0,=str7
-	mov	r1,r12
+	mov	r1,r5
 	bl	printf
 	@end
 	b	end
